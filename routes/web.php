3f7d02b84ebdf8c03457866e4dae646b3a4c36d8
Shungo.Yanase <shungosp@gmail.com>
2018-06-27 01:54:04 +0000

    ranking

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
+//want
+Route::get('ranking/want', 'RankingController@want')->name('ranking.want');
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('items', 'ItemsController', ['only' => ['create', 'show']]);
     Route::post('want', 'ItemUserController@want')->name('item_user.want');
@@ -31,6 +33,7 @@ Route::group(['middleware' => ['auth']], function () {
 
 
 //have
+Route::get('ranking/have', 'RankingController@have')->name('ranking.have');
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('items', 'ItemsController', ['only' => ['create', 'show']]);
     Route::post('have', 'ItemUserController@have')->name('item_user.have');
